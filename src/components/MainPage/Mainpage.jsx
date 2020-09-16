a03d0be787a63d03709f4b969ac53278b140d518
Aditya Morankar <47907947+adi112100@users.noreply.github.com>
2020-09-16 12:24:49 +0530

    added hovering effect

--- src/components/MainPage/Mainpage.jsx
+++ src/components/MainPage/Mainpage.jsx
@@ -40,13 +40,13 @@ function Mainpage() {
                 </div>
             </div>
             <div className="position-absolute icons">
-                <span><i class="fab fa-facebook"></i></span>
-                <span><i class="fab fa-linkedin-in"></i></span>
-                <span><i class="fab fa-github"></i></span>
-                <span><i class="fab fa-twitter"></i></span>
+                <a href="" target="_blank" className="icon_a"><i class="fab fa-facebook"></i></a>
+                <a href="" target="_blank" className="icon_a"><i class="fab fa-linkedin-in"></i></a>
+                <a href="" target="_blank" className="icon_a"><i class="fab fa-github"></i></a>
+                <a href="" target="_blank" className="icon_a"><i class="fab fa-twitter"></i></a>
 
             </div>
-            
+
         </>
     )
 }
