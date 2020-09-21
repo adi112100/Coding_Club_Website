78bbaa64d393aa1882201dd03897052aca057aea
Aditya Morankar <47907947+adi112100@users.noreply.github.com>
2020-09-21 20:55:05 +0530

    added cnew contest api

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,3 +1,5 @@
+// gAAAAABfYyrPc24Rm_-3GlzW0nzgy2kfCHevEb3KnbDEBUwnwqIrBMVhBaTxcf1PS6FgRjSDJ6o1IBcbfhTycyQFuqR3sJn_XQ==
+
 import React, { useState } from 'react'
 import Axios from 'axios'
 import AdminView from './AdminView';
@@ -9,6 +11,7 @@ function Admin() {
     const [htmlbody, setBody] = useState('');
     const [message, fetchMessage] = useState({ message: "", status: "none" });
     const [callbacks, fetchCallbacks] = useState({ message: "", status: "none" });
+    const [contest, setContest] = useState({name:"", platform:"", startTime:"", endTime:"", url:"", typeOfEvent:"", status:""});
 
 
     const submithandler = (e) => {
@@ -49,13 +52,47 @@ function Admin() {
 
     }
 
+    const newcontest = (e)=>
+    {
+        var start = new Date(contest.startTime).toISOString()
+        var end = new Date(contest.endTime).toISOString()
+        console.log(start,end)
+        e.preventDefault()
+        Axios.post("https://o1codingclub.herokuapp.com/contest/", 
+        {
+            name : contest.name,
+            platform : contest.platform,
+            startTime : start,
+            endTime : end,
+            url : contest.url,
+            typeOfEvent : contest.typeOfEvent,
+            status : contest.status,
+            key : key
+
+
+        }).
+            then((response) => {
+
+                fetchCallbacks({ ...callbacks, message: response.data, status: "SUCCESS" })
+
+
+            }).
+            catch(function (error) {
+                
+                console.log(error)
+                fetchCallbacks({ ...callbacks, message: "Unexpected Error occur!!", status: "INVALID" })
+            });
+
+
+    }
+
     const sendmail = (e) => {
         e.preventDefault()
         Axios.post("https://o1codingclub.herokuapp.com/mail/", { HTML: htmlbody, key: key }).
             then((response) => {
 
                 fetchCallbacks({ ...callbacks, message: response.data, status: "SUCCESS" })
-               
+
 
             }).
             catch(function (error) {
@@ -215,13 +252,18 @@ function Admin() {
 
                     <div className="container shadow" style={{ borderRadius: "50px", backgroundColor: "white" }}>
                         <div className="row p-3">
-                            <div className="col-3 col-xl-5"></div>
+                            <div className="col-0 col-xl-4"></div>
                             <div className="col-6 col-xl-2" style={{ display: "block", margin: "auto" }}>
                                 <button type="button" className="btn btn-lg btn-dark" data-toggle="modal" data-target="#sendmail">
                                     <i className="fas fa-mail-bulk"></i>  Mail All
                                 </button>
                             </div>
-                            <div className="col-3 col-xl-5"></div>
+                            <div className="col-6 col-xl-2" style={{ display: "block", margin: "auto" }}>
+                                <button type="button" className="btn btn-lg btn-dark" data-toggle="modal" data-target="#newcontest">
+                                    <i className="fas fa-code"></i>  Contest
+                                </button>
+                            </div>
+                            <div className="col-0 col-xl-4"></div>
 
 
 
@@ -269,6 +311,90 @@ function Admin() {
                                         value={htmlbody}
                                         onChange={(e) => setBody(e.target.value)} />
                                 </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade" id="newcontest" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-lg">
+                    <div className="modal-content" style={{ borderRadius: "50px", padding: "40px" }}>
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Message (HTML format)</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className={`shadow sticky-top alert alert-${callbacks.status === 'SUCCESS' ? 'success' : 'warning'}  fade show ${callbacks.status === 'SUCCESS' ? 'condition2' : 'condition1'}`}
+                                role="alert" style={{ borderRadius: "50px" }}>
+                                <strong>{callbacks.status}</strong> {callbacks.message}
+
+                            </div>
+                            <form onSubmit={newcontest}>
+                                <div className="form-group">
+                                    <label htmlFor="name">Contest Name</label>
+                                    <input type="text" className="form-control" id="name"
+                                        value={contest.name}
+                                        onChange={(e) => setContest({ ...contest, name: e.target.value })}
+                                        
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="platform">Platform</label>
+                                    <input type="text" className="form-control" id="platform"
+                                        value={contest.platform}
+                                        onChange={(e) => setContest({ ...contest, platform: e.target.value })}
+
+                                       
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="startime">Start Time</label>
+                                    <input type="date" className="form-control" id="startime"
+                                        value={contest.startTime}
+                                        onChange={(e) => setContest({ ...contest, startTime: e.target.value })}
+                                        
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="startime">End Time</label>
+                                    <input type="date" className="form-control" id="startime"
+                                        value={contest.endTime}
+                                        onChange={(e) => setContest({ ...contest, endTime: e.target.value })}
+                                        
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="contestlink">Contest URL</label>
+                                    <input type="url" className="form-control" id="contestlink"
+                                        value={contest.url}
+                                        onChange={(e) => setContest({ ...contest, url: e.target.value })}
+                                       
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="typee">Type of Event</label>
+                                    <input type="number" className="form-control" id="typee"
+                                        value={contest.typeOfEvent}
+                                        onChange={(e) => setContest({ ...contest, typeOfEvent: e.target.value })}
+                                        
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="status">Status</label>
+                                    <input type="number" className="form-control" id="status"
+                                        value={contest.status}
+                                        onChange={(e) => setContest({ ...contest, status: e.target.value })}
+                                        
+                                    />
+                                </div>
+
                                 <button type="submit" className="btn btn-primary">Submit</button>
                             </form>
                         </div>
